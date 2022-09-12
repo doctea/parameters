6bed6750dd2b1525d781fd978e5e82be5af351d3
doctea <doctea@gmail.com>
2022-09-12 21:13:51 +0100

    alternative approach to formatting templated datatypes - avoid using ArxTypeTraits+constexpre so that it works on Teensy

--- include/parameters/Parameter.h
+++ include/parameters/Parameter.h
@@ -1,11 +1,15 @@
 #ifndef PARAMETER__INCLUDED
 #define PARAMETER__INCLUDED
 
-#include <ArxTypeTraits.h>
+#include <Arduino.h>
+#ifdef CORE_TEENSY
+    // use alternative method if Teensy
+#else
+    #include <ArxTypeTraits.h>
+#endif
+#include <stddef.h>
 
 extern char NEXT_PARAMETER_NAME;
-
-#include <Arduino.h>
 
 //#include "ParameterInput.h"
 
@@ -95,8 +99,13 @@ class Parameter : public DataParameter {
         void (test::*func)(float);
         void (test::*func)(int);*/
 
-        Parameter(char *label, TargetClass *target, void(TargetClass::*setter_func)(DataType)) : DataParameter(label) {
+        Parameter(char *label, TargetClass *target) : DataParameter(label) {
             this->target = target;
+        }
+        Parameter(char *label, TargetClass *target, DataType initial_value_normal) : Parameter(label, target) {
+            this->initial_value = initial_value_normal;
+        }
+        Parameter(char *label, TargetClass *target, void(TargetClass::*setter_func)(DataType)) : Parameter(label, target) {
             this->setter_func = setter_func;
         }
         Parameter(char *label, TargetClass *target, void(TargetClass::*setter_func)(DataType), double(TargetClass::*getter_func)()) : Parameter(label, target, setter_func) {
@@ -117,7 +126,6 @@ class Parameter : public DataParameter {
             this->minimum_value = minimum_value;
             return this;
         }*/
-        // TODO: this probably isnt legal usage of NULL?
         virtual DataParameter* initialise_values(double minimum_value = 0.0, double maximum_value = 100.0) {
             double current_value_normal = 0.0f;
             if (this->getter_func!=nullptr) 
@@ -125,7 +133,8 @@ class Parameter : public DataParameter {
             return this->initialise_values(minimum_value, maximum_value, current_value_normal);
         }
         virtual DataParameter* initialise_values(double minimum_value, double maximum_value, double current_value_normal) {
-            if (current_value_normal==NULL && this->getter_func!=nullptr) 
+            // TODO: this probably isnt legal usage of NULL?
+            if (/*current_value_normal==NULL && */this->getter_func!=nullptr) 
                 current_value_normal = (this->target->*getter_func)() / maximum_value;
             this->minimum_value = minimum_value;
             this->maximum_value = maximum_value;
@@ -148,6 +157,16 @@ class Parameter : public DataParameter {
             }*/
         }
 
+        virtual void actual_set_value(double value) {
+            if (this->target!=nullptr && this->setter_func!=nullptr) {
+                (this->target->*setter_func)((DataType)value);
+            } else {
+                #ifdef ENABLE_PRINTF
+                    Serial.printf("WARNING: no target / no setter_func in %s!\n", this->label);
+                #endif
+            }
+        }
+
         virtual void setParamValue(double value, double range=1.0) override {
             if (this->currentValue==value) 
                 return;
@@ -165,13 +184,8 @@ class Parameter : public DataParameter {
                     Serial.println(")");
                 }
             #endif   
-            if (this->target!=nullptr && this->setter_func!=nullptr) {
-                (this->target->*setter_func)((DataType)value);
-            } else {
-                #ifdef ENABLE_PRINTF
-                    Serial.printf("WARNING: no target / no setter_func in %s!\n", this->label);
-                #endif
-            }
+
+            this->actual_set_value(value);
         }
         /*void setParamValue(float value) {
             lastValue = currentValue;
@@ -187,20 +201,49 @@ class Parameter : public DataParameter {
             this->target = target;
             this->setter_func = setter_func;
         }*/
-        virtual const char* getFormattedValue() override {
-            static char fmt[20] = "              ";
-            if constexpr (std::is_integral<DataType>::value && std::is_same<DataType, bool>::value) {
+        #ifdef CORE_TEENSY
+            // use Teensy version of this code that uses overriding functions
+            virtual const char* getFormattedValue() override {
+                char *fmt = this->parseFormattedDataType((DataType)this->getCurrentValue());
+                //Serial.printf("getFormattedValue: '%s'\n", fmt);
+                return fmt;
+            };
+            virtual const char* parseFormattedDataType(bool value) {
+                static char fmt[20] = "              ";
                 sprintf(fmt, "%s", this->getCurrentValue()?"On" : "Off");
-            } else if constexpr (std::is_floating_point<DataType>::value) {
-                sprintf(fmt, "%3i%% (float)",     (int)(100.0*this->getCurrentValue())); //->getCurrentValue());
-            } else if constexpr (std::is_unsigned<DataType>::value) {
-                sprintf(fmt, "%5u (unsigned)",    (unsigned int)(this->maximum_value*this->getCurrentValue())); //getCurrentValue());
-            } else {
-                sprintf(fmt, "%5i (signed)",      (int)(this->maximum_value*this->getCurrentValue())); //getCurrentValue());
+                return fmt;
             }
-            //Serial.printf("getFormattedValue: '%s'\n", fmt);
-            return fmt;
-        };
+            virtual const char* parseFormattedDataType(double value) {
+                static char fmt[20] = "              ";
+                sprintf(fmt, "%3i%% (float)",     (int)(100.0*this->getCurrentValue())); //->getCurrentValue());
+                return fmt;
+            }
+            virtual const char* parseFormattedDataType(unsigned int value) {
+                static char fmt[20] = "              ";
+                sprintf(fmt, "%5u (unsigned)",    (unsigned int)(this->maximum_value*this->getCurrentValue())); //getCurrentValue());
+                return fmt;
+            }
+            virtual const char* parseFormattedDataType(int value) {
+                static char fmt[20] = "              ";
+                sprintf(fmt, "%5i (signed)",      (int)(this->maximum_value*this->getCurrentValue())); //getCurrentValue());
+                return fmt;
+            }
+        #else
+            virtual const char* getFormattedValue() override {
+                static char fmt[20] = "              ";
+                if constexpr (std::is_integral<DataType>::value && std::is_same<DataType, bool>::value) {
+                    sprintf(fmt, "%s", this->getCurrentValue()?"On" : "Off");
+                } else if constexpr (std::is_floating_point<DataType>::value) {
+                    sprintf(fmt, "%3i%% (float)",     (int)(100.0*this->getCurrentValue())); //->getCurrentValue());
+                } else if constexpr (std::is_unsigned<DataType>::value) {
+                    sprintf(fmt, "%5u (unsigned)",    (unsigned int)(this->maximum_value*this->getCurrentValue())); //getCurrentValue());
+                } else {
+                    sprintf(fmt, "%5i (signed)",      (int)(this->maximum_value*this->getCurrentValue())); //getCurrentValue());
+                }
+                //Serial.printf("getFormattedValue: '%s'\n", fmt);
+                return fmt;
+            };
+        #endif
 
         virtual void set_target_object(TargetClass *target) {
             this->target = target;
