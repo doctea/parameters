70696790c30466b751b7eef493ba6a6eae82a69b
doctea <doctea@gmail.com>
2024-12-10 00:16:42 +0000

    formatting

--- src/parameters/calibration.cpp
+++ src/parameters/calibration.cpp
@@ -3,114 +3,117 @@
 #include "SimplyAtomic.h"
 #include "ParameterManager.h"
 
+// todo: possibly move this whole thing into CVOutputParameter, or Calibratable...?
+// or like, a DACDevice family of classes 
+
 #include "DAC8574.h"
 extern DAC8574 *dac_output;
 
-    uint16_t calibrate_find_dac_value_for(int channel, VoltageParameterInput *src, float intended_voltage, bool inverted) {
-        parameter_manager->update_voltage_sources();
-        parameter_manager->update_inputs();
-        src->read();
-        parameter_manager->update_voltage_sources();
-        parameter_manager->update_inputs();
-        src->read();
-        src->get_voltage();
+uint16_t calibrate_find_dac_value_for(int channel, VoltageParameterInput *src, float intended_voltage, bool inverted) {
+    parameter_manager->update_voltage_sources();
+    parameter_manager->update_inputs();
+    src->read();
+    parameter_manager->update_voltage_sources();
+    parameter_manager->update_inputs();
+    src->read();
+    src->get_voltage();
 
-        //float intended_voltage = 0.0;
-        int guess_din = (intended_voltage/10.0) * 65535;
-        if (guess_din==0) 
-            guess_din = 3000;
-        else if (guess_din==65535)
-            guess_din = 64000;
-        int last_guess_din = guess_din;
-        float actual_read = 0.0f;
-        float last_read = actual_read;
+    //float intended_voltage = 0.0;
+    int guess_din = (intended_voltage/10.0) * 65535;
+    if (guess_din==0) 
+        guess_din = 3000;
+    else if (guess_din==65535)
+        guess_din = 64000;
+    int last_guess_din = guess_din;
+    float actual_read = 0.0f;
+    float last_read = actual_read;
 
-        bool overshot = false;
-        float tolerance = 0.001;
+    bool overshot = false;
+    float tolerance = 0.001;
 
-        Serial.printf("----\nStarting calibrate_find_dac_value_for(%i, '%s', %3.3f)\n", channel, src->name, intended_voltage);
-        Serial.printf("Starting with guess_din of %i.\n", guess_din);
+    Serial.printf("----\nStarting calibrate_find_dac_value_for(%i, '%s', %3.3f)\n", channel, src->name, intended_voltage);
+    Serial.printf("Starting with guess_din of %i.\n", guess_din);
 
-        //if (inverted) intended_voltage = 10.0 - intended_voltage;
+    //if (inverted) intended_voltage = 10.0 - intended_voltage;
 
-        ATOMIC(){
-            do {
-                dac_output->write(
-                    channel, 
-                    //inverted ? (65535 - guess_din) : 
-                    guess_din
-                );
-                //delay(1);
-                /*
-                parameter_manager->update_voltage_sources();
-                parameter_manager->update_inputs();
-                src->read();
-                parameter_manager->update_voltage_sources();
-                parameter_manager->update_inputs();
-                src->read();
-                */
-                actual_read = src->fetch_current_voltage();
-                actual_read = src->fetch_current_voltage();
-                actual_read = src->fetch_current_voltage();
-                actual_read = inverted ? 10.0 - actual_read : actual_read;   // INVERT THE *READING*
+    ATOMIC(){
+        do {
+            dac_output->write(
+                channel, 
+                //inverted ? (65535 - guess_din) : 
+                guess_din
+            );
+            //delay(1);
+            /*
+            parameter_manager->update_voltage_sources();
+            parameter_manager->update_inputs();
+            src->read();
+            parameter_manager->update_voltage_sources();
+            parameter_manager->update_inputs();
+            src->read();
+            */
+            actual_read = src->fetch_current_voltage();
+            actual_read = src->fetch_current_voltage();
+            actual_read = src->fetch_current_voltage();
+            actual_read = inverted ? 10.0 - actual_read : actual_read;   // INVERT THE *READING*
 
-                if (actual_read > intended_voltage) {
-                    if (last_guess_din < guess_din) {
-                        Serial.println("OVERSHOT 1!");
-                        overshot = true;
-                        tolerance *= 2.0;
-                        Serial.printf("Set tolerance to %3.3f\n", tolerance);
-                    }
-                    last_guess_din = guess_din;
+            if (actual_read > intended_voltage) {
+                if (last_guess_din < guess_din) {
+                    Serial.println("OVERSHOT 1!");
+                    overshot = true;
+                    tolerance *= 2.0;
+                    Serial.printf("Set tolerance to %3.3f\n", tolerance);
+                }
+                last_guess_din = guess_din;
 
-                    guess_din--;
-                } else if (actual_read < intended_voltage) {
-                    if (last_guess_din > guess_din) {
-                        Serial.println("OVERSHOT 2!");
-                        overshot = true;
-                        tolerance *= 2.0;
-                        Serial.printf("Set tolerance to %3.3f\n", tolerance);
-                    }
-                    last_guess_din = guess_din;
-    
-                    guess_din++;
-                } //else {
-                //}
+                guess_din--;
+            } else if (actual_read < intended_voltage) {
+                if (last_guess_din > guess_din) {
+                    Serial.println("OVERSHOT 2!");
+                    overshot = true;
+                    tolerance *= 2.0;
+                    Serial.printf("Set tolerance to %3.3f\n", tolerance);
+                }
+                last_guess_din = guess_din;
 
-                /*if (overshot && fabs(actual_read - intended_voltage) > fabs(last_read - intended_voltage)) {
-                    Serial.println("Breaking because changing found a less-accurate number 1!\n");
-                    guess_din = last_guess_din;
-                    break;
-                } else if (overshot) {
-                    Serial.println("Breaking because changing found a less-accurate number 2!\n");
-                    break;
-                }*/
+                guess_din++;
+            } //else {
+            //}
 
-                //last_guess_din = guess_din;
+            /*if (overshot && fabs(actual_read - intended_voltage) > fabs(last_read - intended_voltage)) {
+                Serial.println("Breaking because changing found a less-accurate number 1!\n");
+                guess_din = last_guess_din;
+                break;
+            } else if (overshot) {
+                Serial.println("Breaking because changing found a less-accurate number 2!\n");
+                break;
+            }*/
 
-                last_read = actual_read;
+            //last_guess_din = guess_din;
 
-                Serial.printf("Finding %3.3f:\t", intended_voltage);
-                Serial.printf("Tried %i\t", last_guess_din);
-                Serial.printf("And got result %3.3f", last_read);
-                if (inverted) Serial.printf(" (inverted)");
-                Serial.println();
+            last_read = actual_read;
 
-                //Serial.printf(" (wanted %3.3f)\n", intended_voltage);
+            Serial.printf("Finding %3.3f:\t", intended_voltage);
+            Serial.printf("Tried %i\t", last_guess_din);
+            Serial.printf("And got result %3.3f", last_read);
+            if (inverted) Serial.printf(" (inverted)");
+            Serial.println();
 
-            } while (fabs(actual_read) - fabs(intended_voltage) > tolerance);
+            //Serial.printf(" (wanted %3.3f)\n", intended_voltage);
 
-            Serial.printf("got actual_read=%3.3f ", actual_read);
-            Serial.printf("(%3.3f distance from intended %3.3f, with final tolerance %3.3f) ", fabs(actual_read) - fabs(intended_voltage), intended_voltage, tolerance);
-            Serial.printf("and last_guess_din=%i\n", last_guess_din);           
-        }
+        } while (fabs(actual_read) - fabs(intended_voltage) > tolerance);
 
-        //return (actual_read/10.0) * 65535.0;
-        return guess_din;
+        Serial.printf("got actual_read=%3.3f ", actual_read);
+        Serial.printf("(%3.3f distance from intended %3.3f, with final tolerance %3.3f) ", fabs(actual_read) - fabs(intended_voltage), intended_voltage, tolerance);
+        Serial.printf("and last_guess_din=%i\n", last_guess_din);           
     }
 
-    uint16_t calibrate_find_dac_value_for(int channel, char *input_name, float intended_voltage, bool inverted) {
-        VoltageParameterInput *src = (VoltageParameterInput*)parameter_manager->getInputForName(input_name);
+    //return (actual_read/10.0) * 65535.0;
+    return guess_din;
+}
 
-        return calibrate_find_dac_value_for(channel, src, intended_voltage, inverted);
-    }
+uint16_t calibrate_find_dac_value_for(int channel, char *input_name, float intended_voltage, bool inverted) {
+    VoltageParameterInput *src = (VoltageParameterInput*)parameter_manager->getInputForName(input_name);
+
+    return calibrate_find_dac_value_for(channel, src, intended_voltage, inverted);
+}
