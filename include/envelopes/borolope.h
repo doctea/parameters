3bb5e247b5b20f36a561bca0b56878cf31b62bf5
doctea <doctea@gmail.com>
2025-01-05 22:58:56 +0000

    move envelopes here from midihelpers library, now that they are based around floats and can be used as parameterinputs

---
+++ include/envelopes/borolope.h
@@ -0,0 +1,299 @@
+#pragma once
+
+// Weirdolope - a minimal one-control envelope generator
+// (c) 2022 Russell Borogove
+// Use at your own risk
+
+// This envelope generator uses floating point math; 
+// if your MCU doesn't have floating-point hardware 
+// support you're gonna have a bad time.
+
+// Call updateEnvelope() periodically at a rate >= 1000 Hz
+
+// Set envelopeState to ENVELOPE_STATE_ATTACK to 
+// begin the envelope and ENVELOPE_STATE_RELEASE to 
+// release it.
+
+// envelopeLevel will range from 0.0 to 1.0. 
+
+// Implement setEnvelope() to scale that value 
+// appropriately and send it to your DAC. 
+
+// Thanks to "synthesizers, yo" user meem for 
+// getting me some nice clean samples of guitars 
+// and pianos that I could measure some 
+// representative envelope time curves from. 
+
+// Now modified quite a bit by doctea/Tristan Rowley with full thanks and royalties ;-) to Russell Borogove!
+
+#include "envelopes.h"
+
+class Weirdolope : public EnvelopeBase {
+
+    private:
+    float AttackRateTable[9] = 
+    {
+        1.00000f,     // instant
+        0.08333f,     // guitar
+        0.04000f,     // bass
+        0.01667f,     // piano bass
+        0.10000f,     // piano treble
+        0.20000f,     // chiff organ
+        0.10000f,     // synth lead
+        0.00250f,     // synth pad
+        0.00050f,     // long ambient pad
+    };
+
+    float DecayRateTable[9] = 
+    {  
+        0.96555f,     // instant
+        0.99929f,     // guitar
+        0.98851f,     // bass
+        0.99934f,     // piano bass
+        0.98746f,     // piano treble
+        0.98000f,     // chiff organ
+        0.99644f,     // synth lead
+        0.99978f,     // synth pad
+        0.99988f,     // long ambient pad   
+    };
+
+    // End-of-decay level
+    float SustainLevelTable[9] = 
+    {
+        0.70f,     // instant
+        0.72f,     // guitar
+        0.75f,     // bass
+        0.70f,     // piano bass
+        0.70f,     // piano treble
+        0.70f,     // chiff organ
+        0.70f,     // synth lead
+        0.80f,     // synth pad
+        0.90f,     // long ambient pad   
+    };
+
+    // Unlike a typical synth ADSR envelope, we use an 
+    // exponentially decaying sustain. 
+    float SustainRateTable[9] = 
+    {
+        0.99880f,     // instant
+        0.99928f,     // guitar
+        0.99957f,     // bass
+        0.99913f,     // piano bass
+        0.99015f,     // piano treble
+        0.99977f,     // chiff organ
+        0.99991f,     // synth lead
+        1.0f,     // synth pad
+        1.0f,     // long ambient pad   
+    };
+
+    // Unlike a typical synth ADSR envelope, we use an 
+    // exponentially decaying sustain
+    float ReleaseRateTable[9] = 
+    {
+        0.99312f,     // instant
+        0.99421f,     // guitar
+        0.99484f,     // bass
+        0.99092f,     // piano bass
+        0.97727f,     // piano treble
+        0.99500f,     // chiff organ
+        0.99500f,     // synth lead
+        0.99650f,     // synth pad
+        0.99800f,     // long ambient pad   
+    };
+
+    static const int RELEASE_MULTIPLIER = 3;    // multiplier for release values, because otherwise they're a bit sillylong
+
+    int EnvA = 0;
+    int EnvB = 1;
+    float EnvAlpha = 0.0f;
+
+    long long lastUpdatedClock = 0l;
+
+    float envelopeLevel = 0.0f;
+    // silence the envelope when it reaches this level, well below 12 bit dac resolution.
+    float envelopeStopLevel = 0.0001f;
+    unsigned long nextEnvelopeUpdate = 0;
+
+    float base_level;
+
+    float paramValueA = 0.0f;
+
+    float lerp( float y0, float y1, float alpha )
+    {
+        return (y1-y0)*alpha + y0;
+    }
+
+    using SetterCallback = void (*)(float,bool);
+    using StateChangeCallback = void (*)(int, int);
+
+    // callback handlers
+    SetterCallback setterCallback;
+    StateChangeCallback stateChangeCallback;
+
+    //bool inverted = false;
+    //float idleSlew = false;
+    float slewRate = 0.0f;
+
+    unsigned long stageStartedAt;
+    float stageStartLevel = 0.0f;
+
+    public:
+
+    Weirdolope(const char *label, setter_func_def setter) : EnvelopeBase(label, setter) {
+        this->setMix(0.5);
+    }
+
+    //int envelopeState = stage_t::OFF;
+
+    bool dirty_calc = false;
+    virtual void set_dirty_graph(bool v = true) {
+        EnvelopeBase::set_dirty_graph(v);
+        if (v) this->dirty_calc = true;
+    }
+    virtual bool is_dirty_calc() {
+        return this->dirty_calc;
+    }
+    virtual void clear_dirty_calc() {
+        this->dirty_calc = false;
+    }
+
+    void setSlewRate(float in_slew = true) {
+        slewRate = in_slew;
+        this->set_dirty_graph();
+    }
+
+    void setMix(float v) {
+        bool should_recalculate = this->paramValueA != v;
+        this->paramValueA = v;
+        if (should_recalculate) {
+            this->set_dirty_graph();
+        }
+    }
+    float getMix() {
+        return this->paramValueA;
+    }
+
+    uint16_t last_processed_elapsed = -1;
+    envelope_state_t cached_state;
+    virtual envelope_state_t calculate_envelope_level(stage_t stage, uint16_t stage_elapsed, float level_start, float velocity = 1.0f, bool use_caching = true) {
+        if (use_caching && last_state.stage==stage && last_processed_elapsed==stage_elapsed && !this->is_dirty_calc())
+            return cached_state;    // todo: need to check dirty flag!
+
+        float x = constrain( 8.0f * (paramValueA/10.0f), 0.0f, 7.999f );
+        EnvA = int(x);
+        EnvB = EnvA+1;
+        EnvAlpha = constrain( x - EnvA, 0.0f, 1.0f );
+
+        float envelopeLevel = ((float)level_start);
+        //float delta, damp;
+
+        envelope_state_t return_state;
+        return_state.stage = stage;
+        return_state.lvl_start = level_start;
+        return_state.elapsed = stage_elapsed;
+
+        if (debug) Serial.printf("%s:\tcalculate_envelope_level(stage=%i,\tstage_elapsed=%i,\tlevel_start=%1.3f,\tvelocity=%1.3f)", this->label, stage, stage_elapsed, level_start, velocity);
+
+        if (stage==OFF) {
+            envelopeLevel = 0.0;
+            if (this->is_loop())
+                return_state.stage = ATTACK;
+            /*envelopeLevel = lerp(
+                //inverted ? 1.0f-stageStartLevel : stageStartLevel, 
+                stageStartLevel,
+                0.0f, 
+                //constrain((bpm_clock()-stageStartedAt)/slewRate, 0.0f, 1.0f)
+                constrain((float)stage_elapsed/slewRate, 0.0f, 1.0f)
+            );*/
+        } else if (stage==ATTACK) {
+            float delta = lerp( AttackRateTable[EnvA], AttackRateTable[EnvB], EnvAlpha );
+            if (debug && Serial) Serial.printf("%s: ATTACK phase, stage_elapsed=%i, envelopeLevel=%1.3f, delta=%1.3f =>", this->label, stage_elapsed, envelopeLevel, delta);
+            envelopeLevel += (delta * (float)(stage_elapsed+1));
+            if (debug && Serial) Serial.printf("%3.3f\n", envelopeLevel);
+            if (envelopeLevel >= 1.0f) {
+                envelopeLevel = 1.0f;
+                return_state.lvl_start = envelopeLevel;
+                return_state.stage = DECAY;
+            }
+        } else if (stage==DECAY) {
+            float damp = lerp( DecayRateTable[EnvA], DecayRateTable[EnvB], EnvAlpha );
+            //envelopeLevel *= damp;
+            //envelopeLevel *= pow(damp,(float)stage_elapsed);
+            for (int i = 0 ; i < stage_elapsed ; i++) {
+                envelopeLevel *= damp;
+            }
+            float sustainLevel = lerp(SustainLevelTable[EnvA], SustainLevelTable[EnvB], EnvAlpha);
+            if (envelopeLevel <= sustainLevel) {
+                return_state.stage = SUSTAIN;
+            }
+        } else if (stage==SUSTAIN) {
+            float damp = lerp( SustainRateTable[EnvA], SustainRateTable[EnvB], EnvAlpha );
+            //envelopeLevel *= pow(damp,(float)stage_elapsed);
+            for (int i = 0 ; i < stage_elapsed ; i++) {
+                envelopeLevel *= damp;
+            }
+            if (envelopeLevel <= envelopeStopLevel) {
+                return_state.stage = RELEASE;
+            }
+        } else if (stage==RELEASE) {
+            float damp = lerp( ReleaseRateTable[EnvA], ReleaseRateTable[EnvB], EnvAlpha );
+            //envelopeLevel *= pow(damp,(float)stage_elapsed);
+            for (int i = 0 ; i < (stage_elapsed * RELEASE_MULTIPLIER) ; i++) {   // modify release time in order to shorten the release time, as its a bit ridiculous otherwise
+                envelopeLevel *= damp;
+            }
+            if (envelopeLevel <= envelopeStopLevel) {
+                if (debug && Serial) Serial.printf("%s:\t!!!! Release staged reached level %1.3f (passing threshold %3.3f) after %i stages\n", this->label, envelopeLevel, envelopeStopLevel, stage_elapsed);
+                return_state.stage = OFF;
+                envelopeLevel = 0.0f;
+            } else {
+                //if (Serial) Serial.printf("%s:\tRelease stage reached level %3.3f after %i stages\n", this->label, envelopeLevel, stage_elapsed);
+            }
+        }
+
+        return_state.lvl_now = envelopeLevel;
+
+        if (debug && Serial) Serial.printf(" => %i & %i from envelopeLevel=%1.3f\n", return_state.lvl_now, return_state.lvl_start, envelopeLevel);
+
+        if (use_caching) {
+            this->clear_dirty_calc();
+            cached_state.elapsed    = return_state.elapsed;
+            cached_state.lvl_now    = return_state.lvl_now;
+            cached_state.lvl_start  = return_state.lvl_start;
+            cached_state.stage      = return_state.stage;
+            last_processed_elapsed  = stage_elapsed;
+        }
+
+        return return_state;
+    }
+
+
+    virtual void randomise() override {
+        this->setMix(random(0.0f, 10.0f));
+        this->set_invert((int8_t)random(0,10) < 2);
+    }
+    virtual void update_state(float velocity, bool state, uint32_t now = ticks) override {
+        if (!state) {
+            if (stage==ATTACK || stage==HOLD || stage==DECAY || stage==SUSTAIN) { //!=OFF && stage!=RELEASE) {
+                last_state.stage = stage = RELEASE;
+                last_state.lvl_start = last_state.lvl_now;
+                triggered_at = stage_triggered_at = now;
+                last_sent_at = 0;
+            }
+        } else {
+            last_state.stage = stage = ATTACK;
+            last_state.lvl_start = last_state.lvl_now;
+            triggered_at = stage_triggered_at = now;
+            last_sent_at = 0;
+        }
+    }
+
+    #ifdef ENABLE_PARAMETERS
+        virtual LinkedList<FloatParameter*> *get_parameters() override;
+    #endif
+
+    #ifdef ENABLE_SCREEN
+        FLASHMEM
+        virtual void make_menu_items(Menu *menu, int index) override;
+    #endif
+
+};
---
+++ include/envelopes/envelopes.h
@@ -0,0 +1,424 @@
+// TODO: envelopes should use float or something as underlying type, rather than poor-resolution int/midi CC like it does currently!
+// TODO: allow envelope outputs to be registered as ParameterInputs so that they can be used as modulation sources
+// TODO: allow Envelopes to have their types dynamically switched... (will be very tricky due to needing different UI..)
+
+#pragma once
+
+#include "bpm.h"
+#include "functional-vlpp.h"
+#include "SinTables.h"
+
+#define SUSTAIN_MINIMUM   0.01f   // was 32         // minimum sustain volume to use (below this becomes inaudible, so cut it off)
+#define ENV_MAX_ATTACK    (PPQN*2) //48 // maximum attack stage length in ticks
+#define ENV_MAX_HOLD      (PPQN*2) //48 // maximum hold stage length
+#define ENV_MAX_DECAY     (PPQN*2) //48 // maximum decay stage length
+#define ENV_MAX_RELEASE   (PPQN*4) //96 // maximum release stage length
+
+//#if defined(ENABLE_SCREEN) // also need this for parameters support, so need a way to tell if that should be activated here
+#include <LinkedList.h>
+//#endif
+
+#ifdef ENABLE_SCREEN
+    class Menu;
+#endif
+#ifdef ENABLE_PARAMETERS
+    class FloatParameter;
+#endif
+
+enum stage_t : int8_t {
+    OFF = 0,
+    ATTACK,
+    HOLD, // time
+    DECAY,
+    SUSTAIN,
+    RELEASE,
+};
+stage_t operator++ (stage_t& d);
+
+class EnvelopeBase { 
+    private:
+    bool loop_mode = false;
+    bool invert = false;
+    bool dirty_graph = true;
+
+    public:
+    bool debug = false;
+    const char *label = nullptr;
+    using setter_func_def = vl::Func<void(float)>;
+    setter_func_def setter;
+
+    EnvelopeBase(const char *label, setter_func_def setter) {
+        this->label = label;
+        this->setter = setter;
+    }
+
+    stage_t stage = OFF;
+    float velocity = 1.0f;         // triggered velocity
+    float actual_level = 0.0f;          // right now, the level
+    unsigned long stage_triggered_at = 0;
+    unsigned long triggered_at = 0; 
+    unsigned long last_sent_at = 0;
+    int trigger_on = 0; // 0->19 = trigger #, 20 = off, 32->51 = trigger #+loop, 64->84 = trigger #+invert, 96->116 = trigger #+loop+invert
+    float last_sent_lvl; // value but not inverted
+    float last_sent_actual_lvl;  // actual midi value sent
+
+    virtual bool is_dirty_graph() {
+        return this->dirty_graph;
+    }
+    virtual void set_dirty_graph(bool v = true) {
+        this->dirty_graph = true;
+    }
+    virtual void clear_dirty_graph() {
+        EnvelopeBase::set_dirty_graph(false);
+    }
+
+    virtual bool is_invert() {
+        return invert;
+    }
+    virtual void set_invert(bool i) {
+        this->invert = i;
+        this->set_dirty_graph();
+    }
+    virtual bool is_loop() {
+        return loop_mode;
+    }
+    virtual void set_loop(bool i) {
+        this->loop_mode = i;
+        this->set_dirty_graph();
+    }
+
+    virtual void send_envelope_level(float level) {
+        if (is_invert())
+            level = 1.0f - level;
+        if (last_sent_actual_lvl != level)
+            this->setter(level);
+        last_sent_actual_lvl = level;
+    };
+
+    virtual float get_envelope_level() {
+        return this->last_sent_actual_lvl;
+    }
+
+    virtual void randomise() = 0;
+
+    virtual void kill() {
+        this->stage = OFF;
+        this->last_state.stage = OFF;
+        this->last_state.lvl_start = 0.0f;
+        this->last_state.lvl_now = 0.0f;
+        send_envelope_level(0.0f);
+    }
+
+    virtual void update_state (float velocity, bool state, uint32_t now = ticks) = 0;
+
+    struct envelope_state_t {
+        stage_t stage = OFF;
+        float lvl_start = 0.0f;
+        float lvl_now = 0.0f;
+        uint32_t elapsed = 0;
+    };
+    virtual envelope_state_t calculate_envelope_level(stage_t stage, uint16_t stage_elapsed, float level_start, float velocity = 1.0f, bool use_caching = true) = 0;
+
+    struct graph_t {
+        float value = 0.0f;
+        stage_t stage = stage_t::OFF;
+    };
+    static const int GRAPH_SIZE = 240;
+    graph_t graph[GRAPH_SIZE];
+
+    virtual void recalculate_graph_if_necessary() {
+        if (this->is_dirty_graph())
+            this->calculate_graph();
+    }
+
+    virtual void calculate_graph() {
+        if (debug) Serial.printf("%s:calculate_graph starting..", this->label);
+        envelope_state_t graph_state = {
+            .stage = ATTACK,
+            .lvl_start = 0.0f,
+            .lvl_now = 0.0f
+        };
+        int stage_elapsed = 0;
+        for (int i = 0 ; i < GRAPH_SIZE ; i++) {
+            envelope_state_t result = calculate_envelope_level(graph_state.stage, stage_elapsed, graph_state.lvl_start, velocity, false);
+            if (result.stage != graph_state.stage) {
+                graph_state.lvl_start = result.lvl_now;
+                stage_elapsed = 0;
+            } else {
+                stage_elapsed++;
+            }
+            graph[i].value = is_invert() ? 1.0f - result.lvl_now : result.lvl_now;
+            graph[i].stage = result.stage;
+            graph_state.stage = result.stage;
+            if (result.stage == SUSTAIN && stage_elapsed >= PPQN) {
+                stage_elapsed = 0;
+                graph_state.stage = RELEASE;    // move to release after 1 beat, if we are calculating the graph
+                graph_state.lvl_start = result.lvl_now;
+            }
+        }
+        if (debug) Serial.printf("%s:calculate_graph finished.", this->label);
+        this->clear_dirty_graph();
+    }
+
+    envelope_state_t last_state = {
+        .stage = OFF,
+        .lvl_start = 0.0f,
+        .lvl_now = 0.0f,
+        .elapsed = 0
+    };
+    virtual void process_envelope(unsigned long now = millis()) {
+        unsigned long elapsed = now - this->stage_triggered_at;
+        envelope_state_t new_state = calculate_envelope_level(last_state.stage, elapsed, last_state.lvl_start, velocity, true);
+        if (new_state.stage != last_state.stage) {
+            new_state.lvl_start = new_state.lvl_now;
+            this->stage_triggered_at = now;
+        }
+        send_envelope_level(new_state.lvl_now);
+        last_state.elapsed = elapsed;
+        last_state.stage = new_state.stage;
+        last_state.lvl_start = new_state.lvl_start;
+        last_state.lvl_now = new_state.lvl_now;
+    }
+
+    #ifdef ENABLE_SCREEN
+        FLASHMEM
+        virtual void make_menu_items(Menu *menu, int index);
+    #endif
+
+    #ifdef ENABLE_PARAMETERS
+        LinkedList<FloatParameter*> *parameters = nullptr;
+        virtual LinkedList<FloatParameter*> *get_parameters() {
+            return nullptr;
+        }
+    #endif
+};
+
+class RegularEnvelope : public EnvelopeBase {
+    public:
+    RegularEnvelope(const char *label, setter_func_def setter) : EnvelopeBase(label, setter) {
+        this->initialise_parameters();
+    }
+
+    unsigned int  attack_length   = 0;                // A - attack  - length of stage
+    unsigned int  hold_length     = (PPQN / 4) - 1;   // H - hold    - length to hold at end of attack before decay
+    unsigned int  decay_length    = (PPQN / 2) - 1;   // D - decay   - length of stage
+    float         sustain_ratio   = 0.90f;            // S - sustain - level to drop to after decay phase
+    unsigned int  release_length  = (PPQN / 2) - 1;   // R - release - length (time to drop to 0)
+
+    int8_t lfo_sync_ratio_hold_and_decay = 0;
+    int8_t lfo_sync_ratio_sustain_and_release = 0;
+    int8_t cc_value_sync_modifier = 12;
+
+    virtual void randomise() {
+        this->set_attack((float)random(0,64) / 127.0f);
+        this->set_hold((float)random(0,127) / 127.0f);
+        this->set_decay((float)random(0,127) / 127.0f);
+        this->set_sustain((float)random(64,127) / 127.0f);
+        this->set_release((float)random(0,127) / 127.0f);
+        this->set_invert((int8_t)random(0,10) < 2);
+    }
+
+    virtual void initialise_parameters() {
+        this->set_attack(0.0f);
+        this->set_hold((float)random(0,127) / 127.0f);
+        this->set_decay((float)random(0,127) / 127.0f);
+        this->set_sustain((float)random(64,127) / 127.0f);
+        this->set_release((float)random(0,127) / 127.0f);
+        this->set_invert((int8_t)random(0,10) < 2);
+    }
+
+    // received a message that the state of the envelope should change (note on/note off etc)
+    virtual void update_state (float velocity, bool state, uint32_t now = ticks) override {
+        if (state == true) {
+            this->velocity = velocity;
+            this->actual_level = velocity;
+            this->stage = ATTACK;
+            last_state.stage = ATTACK;
+            last_state.lvl_start = velocity;
+            this->triggered_at = now;
+            this->stage_triggered_at = now;
+            this->last_sent_at = 0;
+        } else if (state == false && this->stage != OFF) {
+            switch (this->last_state.stage) {
+            case RELEASE:
+                return;
+            case OFF:
+                return;
+            case ATTACK:
+            case HOLD:
+            case DECAY:
+            case SUSTAIN:
+            default:
+                this->stage = last_state.stage = RELEASE;
+                last_state.lvl_start = last_state.lvl_now;
+                this->stage_triggered_at = now;
+                this->last_sent_at = 0;
+                break;
+            }
+        }
+    }
+
+    virtual envelope_state_t calculate_envelope_level(stage_t stage, uint16_t stage_elapsed, float level_start, float velocity = 1.0f, bool use_caching = true) override {
+        float ratio = (float)PPQN / (float)cc_value_sync_modifier;
+        unsigned long elapsed = (float)stage_elapsed * ratio;
+
+        float lvl;
+        envelope_state_t return_state = {
+            stage,
+            level_start,
+            level_start
+        };
+
+        if (stage == ATTACK) {
+            if (attack_length == 0)
+                lvl = velocity;
+            else
+                lvl = (float)velocity * ((float)elapsed / ((float)this->attack_length));
+            return_state.lvl_now = lvl;
+            if (elapsed >= this->attack_length) {
+                return_state = { .stage = ++stage, .lvl_start = lvl, .lvl_now = lvl };
+            }
+        } else if (stage == HOLD && this->hold_length > 0) {
+            lvl = velocity;
+            return_state.lvl_now = lvl;
+            if (elapsed >= hold_length) {
+                return_state = { .stage = ++stage, .lvl_start = lvl, .lvl_now = lvl };
+            }
+        } else if (stage == HOLD || stage == DECAY) {
+            float f_sustain_level = sustain_ratio * velocity;
+            float f_original_level = level_start;
+
+            if (stage == HOLD)
+                return_state.stage = DECAY;
+
+            if (this->decay_length > 0) {
+                float decay_position = ((float)elapsed / (float)(this->decay_length));
+                float diff = (f_original_level - (f_sustain_level));
+                lvl = f_original_level - (diff * decay_position);
+            } else {
+                lvl = f_sustain_level; 
+            }
+            return_state.lvl_now = lvl;
+            if (elapsed >= this->decay_length) {
+                return_state = { .stage = SUSTAIN, .lvl_start = lvl, .lvl_now = lvl };
+            }
+        } else if (stage == SUSTAIN) {
+            return_state.lvl_now = sustain_ratio * velocity;
+            if (sustain_ratio == 0.0f || this->is_loop()) {
+                return_state = { .stage = RELEASE, .lvl_start = return_state.lvl_now, .lvl_now = return_state.lvl_now };
+            }
+        } else if (stage == RELEASE) {
+            if (this->sustain_ratio == 0.0f) {
+            }
+            if (this->release_length > 0) {
+                float eR = (float)elapsed / (float)(this->release_length); 
+                eR = constrain(eR, 0.0f, 1.0f);
+                lvl = (float)level_start * (1.0f - eR);                
+            } else {
+                lvl = 0.0f;
+            }
+            return_state.lvl_now = lvl;
+            if (elapsed > this->release_length || this->release_length == 0 || lvl == 0.0f) {
+                return_state = { .stage = OFF, .lvl_start = lvl, .lvl_now = lvl };
+            }
+        } else if (stage == OFF) {
+            return_state = { .stage = OFF, .lvl_start = 0.0f, .lvl_now = 0.0f };
+        }
+
+        if (stage != OFF) {
+            float lvl = return_state.lvl_now;
+            int sync = (stage == DECAY || stage == HOLD) 
+                            ? this->lfo_sync_ratio_hold_and_decay
+                            : (stage == SUSTAIN || stage == RELEASE)
+                            ? this->lfo_sync_ratio_sustain_and_release 
+                            : -1;
+            if (sync >= 0) {            
+                sync *= 4;
+                float mod_amp = lvl / 4.0f;
+                float mod_result = mod_amp * isin((float)elapsed * PPQN * ((float)sync / 127.0f));
+                lvl = constrain(lvl + mod_result, 0.0f, 1.0f);
+                return_state.lvl_now = lvl;
+            }           
+        } else {
+            if (this->is_loop())
+                return_state.stage = ATTACK;
+        }
+
+        return return_state;
+    }
+
+    float attack_value, hold_value, decay_value, sustain_value, release_value;
+
+    virtual void set_attack(float attack) {
+        this->attack_value = attack;
+        this->attack_length = (ENV_MAX_ATTACK) * attack;
+        this->set_dirty_graph();
+    }
+    virtual float get_attack() {
+        return this->attack_value;
+    }
+    virtual void set_hold(float hold) {
+        this->hold_value = hold;
+        this->hold_length = (ENV_MAX_HOLD) * hold;
+        this->set_dirty_graph();
+    }
+    virtual float get_hold() {
+        return this->hold_value;
+    }
+    virtual void set_decay(float decay) {
+        this->decay_value = decay;
+        decay_length = (ENV_MAX_DECAY) * decay;
+        this->set_dirty_graph();
+    }
+    virtual float get_decay() {
+        return this->decay_value;
+    }
+    virtual void set_sustain(float sustain) {
+        this->sustain_value = sustain;
+        this->sustain_ratio = sustain;
+        this->set_dirty_graph();
+    }
+    virtual float get_sustain() {
+        return this->sustain_value;
+    }
+    virtual void set_release(float release) {
+        this->release_value = release;
+        release_length = (ENV_MAX_RELEASE) * release;
+        this->set_dirty_graph();
+    }
+    virtual float get_release() {
+        return this->release_value;
+    }
+    virtual int8_t get_stage() {
+        return this->stage;
+    }
+    virtual void set_mod_hd(int8_t hd) {
+        this->lfo_sync_ratio_hold_and_decay = hd;
+        this->set_dirty_graph();
+    }
+    virtual int8_t get_mod_hd() {
+        return this->lfo_sync_ratio_hold_and_decay;
+    }
+    virtual void set_mod_sr(int8_t sr) {
+        this->lfo_sync_ratio_sustain_and_release = sr;
+        this->set_dirty_graph();
+    }
+    virtual int8_t get_mod_sr() {
+        return this->lfo_sync_ratio_sustain_and_release;
+    }
+    virtual void set_cc_value_sync_modifier(uint8_t sync) {
+        this->cc_value_sync_modifier = sync;
+    }
+    virtual uint8_t get_cc_value_sync_modifier() {
+        return this->cc_value_sync_modifier;
+    }
+
+    #ifdef ENABLE_PARAMETERS
+        virtual LinkedList<FloatParameter*> *get_parameters() override;
+    #endif
+
+    #ifdef ENABLE_SCREEN
+        FLASHMEM
+        virtual void make_menu_items(Menu *menu, int index) override;
+    #endif
+};
