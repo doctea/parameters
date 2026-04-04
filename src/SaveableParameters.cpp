8dae9f39b17f2739867ab5910f24dd5ce55e796f
Tristan Rowley <doctea@gmail.com>
2026-04-04 10:41:52 +0100

    moved SaveableParameter to saveloadlib...

--- src/SaveableParameters.cpp
+++ src/SaveableParameters.cpp
@@ -1,6 +1,6 @@
-#include "SaveableParameters.h"
+// #include "SaveableParameters.h"
 
-const char *true_label = "true";
-const char *enable_label = "enabled";
-const char *warning_label = " - WARNING: no target nor getter func!";
-const char *nop_label = "; nop";
+// const char *true_label = "true";
+// const char *enable_label = "enabled";
+// const char *warning_label = " - WARNING: no target nor getter func!";
+// const char *nop_label = "; nop";
