d494c646e211af0e238b817d7ba6311777d2b4c2
Tristan Rowley <doctea@gmail.com>
2026-06-07 14:27:24 +0100

    tweak calibration/correction on rp2350 - altho doesnt make a noticeable improvement

--- include/voltage_sources/ArduinoPinVoltageSource.h
+++ include/voltage_sources/ArduinoPinVoltageSource.h
@@ -48,20 +48,83 @@ class ArduinoPinVoltageSource : public ADSVoltageSourceBase {
 
             int value = (value1+value2+value3) / 3;
 
+            // if we're on RP2350, apply correction from https://github.com/kitanokitsune/rp2040adc_correction
+            #ifdef ARDUINO_ARCH_RP2350
+                value = this->rp2350adc_correction(value);
+            #endif
+
             if (this->invert) 
                 value = 1023 - value;
 
             float voltage_range = this->maximum_input_voltage - this->minimum_input_voltage;
 
-            //float voltageFromAdc = ((float)value/1024.0) * this->maximum_input_voltage;
-
-            float voltageFromAdc = ((float)value/1024.0) * voltage_range + this->minimum_input_voltage;
+            float voltageFromAdc = ((float)value/1023.0) * voltage_range + this->minimum_input_voltage;
             this->_pre_correction_sample = voltageFromAdc;  // cached for fetch_calibration_sample()
             
             return this->get_corrected_voltage(voltageFromAdc);
         }
         // fetch_calibration_sample() inherited from ADSVoltageSourceBase: returns _pre_correction_sample.
         // compute_calibration() inherited from ADSVoltageSourceBase (linear OLS model).
+        
+        int rp2350adc_correction(int d) {
+            /*
+            rp2350 ADC Correction
+            d     : 12 bit ADC readout (0~4095)
+            return: corrected ADC value
+            */
+            int y;
+            if (d >= 4079) {
+                y = 4095;
+            } else if (d >= 3905) {
+                y = d + 16;
+            } else if (d >= 3839) {
+                y = d + 15;
+            } else if (d >= 3489) {
+                y = d + 14;
+            } else if (d >= 3327) {
+                y = d + 13;
+            } else if (d >= 3105) {
+                y = d + 12;
+            } else if (d >= 2816) {
+                y = d + 11;
+            } else if (d >= 2783) {
+                y = d + 10;
+            } else if (d >= 2559) {
+                y = d + 9;
+            } else if (d >= 2304) {
+                y = d + 10;
+            } else if (d >= 2144) {
+                y = d + 9;
+            } else if (d >= 2047) {
+                y = d + 8;
+            } else if (d >= 1824) {
+                y = d + 9;
+            } else if (d >= 1792) {
+                y = d + 8;
+            } else if (d >= 1535) {
+                y = d + 7;
+            } else if (d >= 1280) {
+                y = d + 8;
+            } else if (d >= 1089) {
+                y = d + 7;
+            } else if (d >= 833) {
+                y = d + 6;
+            } else if (d >= 767) {
+                y = d + 5;
+            } else if (d >= 576) {
+                y = d + 4;
+            } else if (d >= 319) {
+                y = d + 3;
+            } else if (d >= 255) {
+                y = d + 2;
+            } else if (d >= 160) {
+                y = d + 1;
+            } else {
+                y = d;
+            }
+
+            return y;
+        }
 
 };
 
